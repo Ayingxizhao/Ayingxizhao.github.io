ff131079d006ed34e6f301b1457de67ec8314609
Ayingxizhao <zhaopeter580@gmail.com>
2026-01-24 23:21:49 +0000

    Deploying to gh-pages from @ Ayingxizhao/Ayingxizhao.github.io@1591bab85eefe45a8264160578f848bd139265f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,14 +25,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "",
+          description: "a collection of my projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "A collection of my GitHub repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
